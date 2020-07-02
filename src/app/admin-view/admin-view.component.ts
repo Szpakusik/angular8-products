70e0c0f5c3cd9f1c8db8833d9804a0c793b0609b
Szpakusik <szpakusik@gmail.com>
2020-07-02 20:38:25 +0200

    odd product syling + cleaning

--- src/app/admin-view/admin-view.component.ts
+++ src/app/admin-view/admin-view.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-admin-view',
   templateUrl: './admin-view.component.html',
-  styleUrls: ['./admin-view.component.css']
+  styleUrls: ['./admin-view.component.scss']
 })
 export class AdminViewComponent implements OnInit {
 
